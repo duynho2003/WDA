66da28374b301c563a432d74096a62c29e7e52ff
Tommy Nguyen <khenchaynguyen@gmail.com>
2023-09-22 14:56:30 +0700

    up

---
+++ Lab02/Models/Student.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Lab02.Models
+{
+    [Table("tbStudent")]
+    public class Student
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public string code { get; set; }
+        [Required(ErrorMessage ="Student name is required...")]
+        [StringLength(50)]
+        public string? name { get; set; }
+        [Required(ErrorMessage = "Student address is required...")]
+        [StringLength(50)]
+        public string? address { get; set; }
+        public string? phone { get; set; }
+
+    }
+}
