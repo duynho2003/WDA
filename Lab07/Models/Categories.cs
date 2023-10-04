da68747509a74f98ce3410c87e93fc982291f8ff
Tommy Nguyen <khenchaynguyen@gmail.com>
2023-10-04 14:27:52 +0700

    up lab07

---
+++ Lab07/Models/Categories.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Lab07.Models
+{
+    public class Categories
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int CategoryID { get; set; }
+        [Required]
+        [StringLength(25)]
+
+        public string? CategoryName { get; set; }
+        
+        [Required]
+        [StringLength(255)]
+        public string? Description { get; set; }
+
+        public ICollection<Products>? Products { get; set; }
+    }
+}
---
+++ Lab07/Models/Products.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Lab07.Models
+{
+    public class Products
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ProductID { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string? ProductName { get; set; }
+
+        public int CategoryID { get; set; } //FK
+        
+        [StringLength(25)]
+        public string? Unit { get; set; }
+        public decimal Price { get; set; }
+
+        public Categories? Categories { get; set; }
+    }
+}
