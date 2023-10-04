d81b0971eaf011dd78b351fb7489b1f267ae8695
Tommy Nguyen <khenchaynguyen@gmail.com>
2023-10-04 16:40:19 +0700

    rename CategoriesProduct -> CategoriesProducts

--- Lab07/Models/CategoriesProducts.cs
+++ Lab07/Models/CategoriesProducts.cs
@@ -1,6 +1,6 @@
 ﻿namespace Lab07.Models
 {
-    public class CategoriesProduct
+    public class CategoriesProducts
     {
         public virtual Categories? Categories { get; set; }
         public virtual Products? Products { get; set; }
