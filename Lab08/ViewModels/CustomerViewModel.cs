956091011ab20d11c950cae0fd25bcd33f3e1621
Tommy Nguyen <khenchaynguyen@gmail.com>
2023-10-06 16:46:41 +0700

    up lab08

--- Lab08/ViewModels/CustomerViewModel.cs
+++ Lab08/ViewModels/CustomerViewModel.cs
@@ -2,11 +2,11 @@
 {
     public class CustomerViewModel
     {
-    {
         public int CustomerId { get; set; }
         public string? CustomerName { get; set; }
         public string? Phone { get; set; }
         public string? Address { get; set; }
         public string? Email { get; set; }
+        public string? ConfirmEmail { get; set; }
     }
 }
