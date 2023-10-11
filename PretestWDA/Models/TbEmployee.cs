3970f268528a3611ea039d0cf3d3c93a84325f47
Tommy Nguyen <khenchaynguyen@gmail.com>
2023-10-11 20:53:02 +0700

    up required

--- PretestWDA/Models/TbEmployee.cs
+++ PretestWDA/Models/TbEmployee.cs
@@ -8,12 +8,12 @@ public partial class TbEmployee
 {
     [Key]
     public int? EmpId { get; set; }
-
+    [Required]
     public DateTime? EmpDob { get; set; }
-
+    [Required]
     public string? EmpName { get; set; }
-
+    [Required]
     public string? EmpAddress { get; set; }
-
+    [Required]
     public string? EmpEmail { get; set; }
 }
