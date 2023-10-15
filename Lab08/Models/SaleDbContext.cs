ca0f7b07d172a188819ddb928f27c21c927a6a26
Tommy Nguyen <khenchaynguyen@gmail.com>
2023-10-16 00:57:52 +0700

    up

--- Lab08/Models/SaleDbContext.cs
+++ Lab08/Models/SaleDbContext.cs
@@ -20,7 +20,6 @@ public partial class SaleDbContext : DbContext
     public virtual DbSet<Order> Orders { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
         => optionsBuilder.UseSqlServer("server=LAPTOP-PH1AFEK8\\SQLEXPRESS;Database=SaleDB;uid=sa;pwd=123;TrustServerCertificate=true;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
