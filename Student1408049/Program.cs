0930acf5a4ae487c7b9096325fe445e64d7f32a9
Tommy Nguyen <khenchaynguyen@gmail.com>
2023-10-17 22:36:42 +0700

    up

---
+++ Student1408049/Program.cs
@@ -0,0 +1,25 @@
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddSession(); //khai bao session
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+app.UseSession(); //dung session
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
