c51537846b75a918713327d45fe1094c965d711b
Tommy Nguyen <khenchaynguyen@gmail.com>
2023-10-18 15:12:30 +0700

    up Student1408049 (Exam 90/100)

--- Student1408049/Controllers/ProjectController.cs
+++ Student1408049/Controllers/ProjectController.cs
@@ -1,12 +1,90 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Student1408049.Models;
 
 namespace Student1408049.Controllers
 {
     public class ProjectController : Controller
     {
+        private ProjectDbContext _context;
+        public ProjectController(ProjectDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
+            return View(_context.TbProjects.ToList());
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
             return View();
+        }
+        [HttpPost]
+        public IActionResult Create(TbProject project)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _context.TbProjects.Add(project);
+                    _context.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception) 
+            {
+                throw;
+            }
+            return View();
+        }
+        public IActionResult UpdateOrRemove(int id)
+        {
+            var project = _context.TbProjects.SingleOrDefault(e => e.Id == id);
+            if (project == null)
+            {
+                return NoContent();
+            }
+            else
+            {
+                return View(project);
+            }
+        }
+        [HttpPost]
+        public IActionResult UpdateOrRemove(TbProject project, string submits) 
+        {
+            try
+            {
+                var proj = _context.TbProjects.SingleOrDefault(e => e.Id.Equals(project.Id));
+                if (submits.Equals("Update"))
+                {
+                    if (proj != null)
+                    {
+                        proj.ClientName = project.ClientName;
+                        proj.ProjectName = project.ProjectName;
+                        proj.StartDate = project.StartDate;
+                        proj.EndDate = project.EndDate;
+                        proj.Cost = project.Cost;
+                        _context.SaveChanges();
+                        return RedirectToAction("Index");
+                    }
+                    else
+                    {
+                        return NotFound();
+                    }
+                }
+                else
+                {
+                    var projDel = _context.TbProjects.SingleOrDefault(e => e.Id.Equals(project.Id));
+                    if (projDel != null)
+                        _context.TbProjects.Remove(projDel);
+                    _context.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
         }
     }
 }
--- Student1408049/Models/TbProject.cs
+++ Student1408049/Models/TbProject.cs
@@ -1,19 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Student1408049.Models;
 
 public partial class TbProject
 {
     public int Id { get; set; }
-
+    [Required]
     public string? ClientName { get; set; }
-
+    [Required]
     public string? ProjectName { get; set; }
-
+    [Required]
     public DateTime? StartDate { get; set; }
-
+    [Required]
     public DateTime? EndDate { get; set; }
-
+    [Required]
+    [Range(999, 100000, ErrorMessage = "Cost must be greater than $1000")]
     public decimal? Cost { get; set; }
 }
